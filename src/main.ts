8fccb62b15b82e42e83cccb517cf86126c2aee54
ilya <i@m.ru>
2023-12-02 20:03:29 +0500

    fetch

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
+import { OpenAPIV2 } from 'openapi-types';
+
+const u = 'https://localhost:7182/swagger/v1/swagger.json';
+
 // import { analytics } from './modules/analytics.js'
-import fetch from 'node-fetch';
 // path.join
+// const u1 = 'http://petstore.swagger.io/v2/swagger.json';
+//
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-// fetch('https://localhost:7182/swagger/v1/swagger.json').then((console.log));
+const r = await fetch(u);
 
-const message: string = 'Hello node!';
-console.log(message);
+const document: OpenAPIV2.Document = (await r.json()) as OpenAPIV2.Document;
+
+const paths = Object.keys(document.paths);
+console.log(document.paths[paths[0]].get?.security);
+
+console.log(document.tags);
+
+// console.log(document.definitions?.WeatherForecast.required);
+
+// console.log(JSON.stringify(t));
+
+// const message: string = 'Hello node!';
+// console.log(message);
 
 // analytics('test')
-const f = async () => {
-  const res = await fetch('https://localhost:7182/swagger/v1/swagger.json');
-  const a = await res.text();
-  console.log(a);
-};
-
-f();
+// const f = async () => {
+//   const res = await fetch('https://localhost:7182/swagger/v1/swagger.json');
+//   const a = await res.text();
+//   console.log(a);
+// };
+//
+// f();
