694371a8454225a43f55fcea825a0a3824aebc5f
ilya <i@m.ru>
2023-12-02 15:18:38 +0500

    Start

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 // import { analytics } from './modules/analytics.js'
+import fetch from 'node-fetch';
+// path.join
 
-const message: string = 'Hello node!'
-console.log(message)
+// fetch('https://localhost:7182/swagger/v1/swagger.json').then((console.log));
+
+const message: string = 'Hello node!';
+console.log(message);
 
 // analytics('test')
+const f = async () => {
+  const res = await fetch('https://localhost:7182/swagger/v1/swagger.json');
+  const a = await res.text();
+  console.log(a);
+};
+
+f();
