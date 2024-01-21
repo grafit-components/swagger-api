a501e0cd4fe6d30f3a791f6cc34873cd257df483
ilya <ilyafill@mail.ru>
2024-01-21 12:22:40 +0500

    Dubles paths

--- src/templates/endpoints/endpoint-paths-template.ts
+++ src/templates/endpoints/endpoint-paths-template.ts
@@ -4,15 +4,17 @@ import { toCamelCase } from '../../utils/string-converters';
 export function makePaths(tagGroupItem: TagGroupItem, endpointsUrlPrefix: string) {
   let controller = '';
   const tag = tagGroupItem.tag;
-  const paths = tagGroupItem.operations.map((operation) => {
+
+  const paths = new Set<string>();
+  tagGroupItem.operations.forEach((operation) => {
     const path = operation.path;
     if (!controller && path.includes(`/${tag}/`)) {
       controller = `_controller: '${endpointsUrlPrefix}${path.split(`/${tag}/`)[0]}/${tag}', `;
     }
-    return `${getPathName(tag, path)}: '${endpointsUrlPrefix}${path}'`;
+    paths.add(`${getPathName(tag, path)}: '${endpointsUrlPrefix}${path}'`);
   });
 
-  return `_paths: { ${controller}${paths.join(', ')} }`;
+  return `_paths: { ${controller}${Array.from(paths).join(', ')} }`;
 }
 export function getPathName(tag: string, path: string) {
   if (path.includes(`/${tag}/`)) {
